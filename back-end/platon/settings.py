bb851d6feabfd3af835b595d1e77322ac1a6e141
yohan_rameau <yohan.rameau@gmail.com>
2022-04-26 16:49:48 +0200

    feat(authentication): add simplejwttoken framework - add some tests - add user model

--- back-end/platon/settings.py
+++ back-end/platon/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import sys
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+APPS_DIR = os.path.realpath(os.path.join(BASE_DIR, "apps"))
 
+if APPS_DIR not in sys.path:  # pragma: no cover
+    sys.path.append(APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
