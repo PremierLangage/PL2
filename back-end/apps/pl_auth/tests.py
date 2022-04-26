e7d88bd3b2c599572b5f6075f0b90fb782d4ca04
yohan_rameau <yohan.rameau@gmail.com>
2022-04-26 16:49:48 +0200

    feat(authentication): add simplejwttoken framework - add some tests - add user model

--- back-end/apps/pl_auth/tests.py
+++ back-end/apps/pl_auth/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from.models import User
 
-# Create your tests here.
+
+class PLAuthTest(TestCase):
+    
+    def setUp(self):
+            self.pascal = User.objects.create_user('pascal', 'pascal@test.com', 'pascal')
+            self.john = User.objects.create_user('john', 'john@test.com', 'john')
+            self.jimmi = User.objects.create_user('jimmi', 'jimmi@test.com', 'jimmi')
+            self.gaston = User.objects.create_user('gaston', 'gaston@test.com', 'gaston')
+
+    def test_simple_authentication_sucess(self):
+        self.assertEquals(self.client.login(username='pascal', password='pascal'), True)
+        self.client.logout()
+        
+    def test_simple_authentication_failure(self):
+        self.assertEquals(self.client.login(username='john', password='pascal'), False)
+        self.client.logout()
+        
+    
+        
+
+        
