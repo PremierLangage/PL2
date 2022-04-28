c50bd3c60e5736e6d3889b00ce911924f4e7bdfa
Anna080 <annadiaw2000@gmail.com>
2022-04-28 17:21:17 +0200

    feat:"First shape activity"

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { ActivityComponent } from './activity/activity.component';
+import { ActivityService} from './activity/activity.service';
 import { CourseListComponent } from './course-list/course-list.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  declarations: [AppComponent, CourseComponent, CourseListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, CourseComponent, ActivityComponent,CourseListComponent],
+  imports: [BrowserModule, BrowserAnimationsModule,AppRoutingModule, HttpClientModule,MatCardModule,MatExpansionModule,MatSidenavModule,MatCheckboxModule,MatIconModule],
+  providers: [ActivityService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
