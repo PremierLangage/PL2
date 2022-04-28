27aa6bafd25592ab464c2a9343468bb26c007b59
bkey <uyarorhan77@gmail.com>
2022-04-28 17:24:01 +0200

    init: assets

--- back-end/apps/pl_assets/models.py
+++ back-end/apps/pl_assets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class AbstractAsset(models.Model):
+
+    name = models.CharField(max_length=50, primary_key=True, blank=False)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_fermeture = models.DateField(blank=True)
+    auteur = models.ForeignKey(get_user_model(), null=False, blank=False)
+    path = models.CharField(max_length=1024, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class AssetBuilder(models.Mode):
+    pass
+
+class Exercice(AbstractAsset):
+    pass
+
+class Activity(AbstractAsset):
+    pass
+
+class Cours(AbstractAsset):
+    pass
+
