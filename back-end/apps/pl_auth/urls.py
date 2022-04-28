3b12cec5887d63b13122b4d738d1ae07a333d773
yohan_rameau <yohan.rameau@gmail.com>
2022-04-28 09:51:49 +0200

    feat(authentication) add auth features : - login - logout - users info @YohanRameau

--- back-end/apps/pl_auth/urls.py
+++ back-end/apps/pl_auth/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView, LogoutAPIView, UserRetrieveUpdateAPIView
 
 app_name = 'pl_auth'
 
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view(), name='register_user'),
+    path('login/', LoginAPIView.as_view(), name='login_user'),
+    path('logout/', LogoutAPIView.as_view(), name="logout_user"),
+    path('user/', UserRetrieveUpdateAPIView.as_view(), name='user'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- back-end/platon/settings.py
+++ back-end/platon/settings.py
@@ -140,5 +140,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST FRAMEWORK
-REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': (
-    'rest_framework_simplejwt.authentication.JWTAuthentication',)}
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',)
+    }
