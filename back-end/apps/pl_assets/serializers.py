91a8e3cca81a48ea313c9a2caa8baf509405d402
bkey <uyarorhan77@gmail.com>
2022-05-04 22:33:46 +0200

    update: assets views

--- back-end/apps/pl_assets/serializers.py
+++ back-end/apps/pl_assets/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from . import models
+from pl_properties.models import AssetProperties
+from pl_properties.serializers import PropertiesSerializer
 
 class AssetSerialize(serializers.ModelSerializer[models.Asset]):
     
+    properties = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = models.Asset
         fields = '__all__'
     
+    def get_properties(self, value : models.Asset):
+        request = self.context['request']
+        queryset = AssetProperties.objects.filter(asset=value)
+        properties = list()
+        for asset in queryset:
+            query = PropertiesSerializer(queryset=asset).to_representation(asset.property)
+            properties.append(query)
+        return properties
--- back-end/apps/pl_properties/serializers.py
+++ back-end/apps/pl_properties/serializers.py
@@ -2,13 +2,25 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from . import models
 
+class AssetDescriptionSerializer(serializers.ModelSerializer[models.AssetDescription]):
+
+    class Meta:
+        model = models.AssetDescription
+        fields = '__all__'
+
+class PropertiesSerializer(serializers.RelatedField):
+    def to_representation(self, value):
+        if isinstance(value, models.AssetDescription):
+            return AssetDescriptionSerializer(value).data
+        raise Exception('Unexpected type of tagged object')
+
 class AssetPropertiesSerializer(serializers.ModelSerializer[models.AssetProperties]):
     
     asset_url = serializers.SerializerMethodField(read_only=True)
     property_url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = models.AssetProperties
-        fields = ['id', 'asset_url', 'property_url', 'asset', 'property_type', 'property_id']
+        fields = ['id', 'asset_url', 'asset', 'property_url', 'property_type', 'property_id']
     
     def get_asset_url(self, value : models.AssetProperties):
         request = self.context['request']
@@ -21,11 +33,4 @@ class AssetPropertiesSerializer(serializers.ModelSerializer[models.AssetProperti
     def get_property_url(self, value : models.AssetProperties):
         request = self.context['request']
         property = value.property
-        return property.get_url(request=request, id = property.pk)    
-
-class AssetDescriptionSerializer(serializers.ModelSerializer[models.AssetDescription]):
-
-    #asset = serializers.PrimaryKeyRelatedField(queryset=models.Asset.objects.all(), write_only=True)
-    class Meta:
-        model = models.AssetDescription
-        fields = '__all__'
+        return property.get_url(request=request, id = property.pk)  
