8113c4d089fca92b313d22fb21d0d334999ee739
bkey <uyarorhan77@gmail.com>
2022-05-04 10:09:26 +0200

    fix: properties

--- back-end/apps/pl_properties/serializers.py
+++ back-end/apps/pl_properties/serializers.py
@@ -3,15 +3,9 @@ from .models import AssetDescription, AssetProperties
 
 class AssetPropertiesSerializer(serializers.ModelSerializer[AssetProperties]):
     
-    content_type = serializers.SerializerMethodField(method_name='content_type_validator')
-
     class Meta:
         model = AssetProperties
         fields = '__all__'
-    
-    def content_type_validator(self, object):
-        print(type(object.content_type))
-        return str(object.content_type)
 
 class AssetDescriptionSerializer(serializers.ModelSerializer[AssetDescription]):
 
