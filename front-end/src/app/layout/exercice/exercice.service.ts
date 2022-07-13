b58a49cdede90406d50c9fa6f13503526193eb91
Antonin <aleronraider@gmail.com>
2022-07-13 16:37:03 +0200

    feat(exercice): cleanse of exercice's component's imports

--- front-end/src/app/layout/exercice/exercice.service.ts
+++ front-end/src/app/layout/exercice/exercice.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { formState } from './models/exercice';
-import { cexExercice } from './models/constUsed/constUsed';
 
 
 @Injectable({
