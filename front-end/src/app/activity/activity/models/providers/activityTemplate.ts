c38fc570c00fae65dbdd03d19236cebe2cf91606
Antonin <aleronraider@gmail.com>
2022-10-24 11:19:14 +0200

    feat(activity): Factorizind the type for later use

---
+++ front-end/src/app/activity/activity/models/providers/activityTemplate.ts
@@ -0,0 +1,5 @@
+import { DefaultComponent } from "../../resources/templates/components/default/default.component";
+
+export declare type ProviderActivityTemplateTypes = DefaultComponent;
+
+export declare type ProviderActivityTemplateKeys = "default" | string;
---
+++ front-end/src/app/activity/activity/models/providers/exercice-list.ts
@@ -0,0 +1,5 @@
+import { DefaultComponent } from "../../resources/exercice-list/components/default/default.component";
+
+export declare type ProviderExerciceListTemplateTypes = DefaultComponent;
+
+export declare type ProviderExerciceListTemplateKeys = "default" | string;
